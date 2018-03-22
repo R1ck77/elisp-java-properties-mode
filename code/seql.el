267c28150ee1cdf6cf253aff9c79279faec3f2ff
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-03-22 08:54:05 +0100

    Add helper function to recurse a file's content

--- code/seql.el
+++ code/seql.el
@@ -1,9 +1,20 @@
-(defun seql-for-each-line-bondaries (function)
-  (goto-char (point-min))
-  (let ((keep-going t))
-    (while keep-going
-      (setq keep-going (= (forward-line) 0))
-      (funcall function (line-beginning-position)
-               (line-end-position)))))
+(require 'cl)
+
+(defun seql-for-each-line-boundaries (consumer)
+  "Recurse each line in the buffer and apply the function consumer to each one, passing the line boundaries as arguments"
+  (save-excursion
+    (goto-char (point-min))
+    (let ((keep-going t))
+      (while keep-going
+        (setq keep-going (= (forward-line) 0))
+        (funcall consumer (line-beginning-position)
+                 (line-end-position))))))
+
+(defun seql-for-each-line-content (consumer)
+  "Helper function that applies the con"
+  (lexical-let ((consumer consumer))
+    (seql-for-each-line-boundaries
+     (lambda (begin end)
+       (funcall consumer (buffer-substring begin end))))))
 
 (provide 'seql)
