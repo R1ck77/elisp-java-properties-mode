da7e0235fce44c59dd50931f3ddd517595d1d78c
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-03-22 08:31:32 +0100

    Minor changes

--- code/seql.el
+++ code/seql.el
@@ -1,7 +1,9 @@
-(defun for-each-line-index (function)
+(defun seql-for-each-line-bondaries (function)
   (goto-char (point-min))
   (let ((keep-going t))
     (while keep-going
       (setq keep-going (= (forward-line) 0))
       (funcall function (line-beginning-position)
                (line-end-position)))))
+
+(provide 'seql)
