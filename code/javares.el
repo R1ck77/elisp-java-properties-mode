b209526a867b1992badffb4887935eec78de9b72
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-03-27 08:25:39 +0200

    Extract simpler functionality from javares--parse-current-resource

--- code/javares.el
+++ code/javares.el
@@ -67,14 +67,20 @@ It either uses \"resources-base-path\" or the base path found using the current 
             (message (concat "Error locating the resource: \"" candidate-path "\" is not readable!"))
             nil)))))
 
-(defun javares--parse-current-resource ()
+(defun javares--current-resource-limits ()
   "Read the current line and return a cons cell with the key and value"
   (save-excursion
     (beginning-of-line)
     (save-match-data
-      (if (search-forward-regexp "^[[:blank:]]*\\(.*?\\)[[:blank:]]*=[[:blank:]]*\\(.*?\\)[[:blank:]]*$" nil t)
-          (cons (match-string 1) (match-string 2))))))
+      (when (search-forward-regexp "^[[:blank:]]*\\(.*?\\)[[:blank:]]*=[[:blank:]]*\\(.*?\\)[[:blank:]]*$" nil t)
+        (list (match-beginning 1) (match-end 1)
+              (match-beginning 2) (match-end 2))))))
 
+(defun javares--parse-current-resource ()
+  "Read the current line and return a cons cell with the key and value"
+  (let ((limits (javares--current-resource-limits)))
+    (cons (buffer-substring (nth 0 limits) (nth 1 limits))
+          (buffer-substring (nth 2 limits) (nth 3 limits)))))
 
 (defun javares--apply-on-current-line (text-highlighter)
   (save-excursion
