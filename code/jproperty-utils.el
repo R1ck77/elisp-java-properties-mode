2b1f4440e54974dc00cf488b34c50a87304b0ee2
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-03-29 08:46:58 +0200

    Import more code from javares to jproperty-utils

--- code/jproperty-utils.el
+++ code/jproperty-utils.el
@@ -1,13 +1,6 @@
 (require 'seq)
 
-(defun javares--contains-java-and-resources-p (path)
-  "Check whether the current path contains both a resources and a java path"
-  (= 2 (length (seq-filter (lambda (component)
-                      (or (equal component "resources")
-                          (equal component "java")))
-                    (directory-files path)))))
-
-(defun javares--drop-last-element (path)
+(defun jproperty-utils--drop-last-element (path)
   "Remove the last component from the path, until the path becomes a \"\"
 
 Works only on absolute files"
@@ -15,40 +8,36 @@ Works only on absolute files"
       (error "drop-last-elemnt works only with absolute paths")
     (mapconcat 'identity (reverse (cdr (reverse (split-string path "/")))) "/")))
 
-(defun javares--drop-elements-until-directory (path)
-  (setq path (javares--drop-last-element path))
+(defun jproperty-utils--drop-elements-until-directory (path)
+  (setq path (jproperty-utils--drop-last-element path))
   (while (or (equal "" path)
              (not (file-accessible-directory-p path)))
-    (setq path (javares--drop-last-element path)))
+    (setq path (jproperty-utils--drop-last-element path)))
   path)
 
-(defun javares--find-code-basepath (path)
+(defun jproperty-utils-tree-common-ancestor-p (path)
+  "Check whether the current path contains both a resources and a java path"
+  (= 2 (length (seq-filter (lambda (component)
+                      (or (equal component "resources")
+                          (equal component "java")))
+                    (directory-files path)))))
+
+(defun jproperty-utils--find-code-basepath (path)
   "Find the first directory that contains both a \"java\" and \"resources\" directory"
-  (setq path (javares--drop-elements-until-directory path)) ; remove the filename
+  (setq path (jproperty-utils--drop-elements-until-directory path)) ; remove the filename
   (let ((path (file-truename path)))
-    (while (not (javares--contains-java-and-resources-p path))
-      (setq path (javares--drop-last-element path))
+    (while (not (jproperty-utils-tree-common-ancestor-p path))
+      (setq path (jproperty-utils--drop-last-element path))
       (if (equal path "") (error "no resources path ")))
     path))
 
-(defun javares--find-code-basepath (path)
-  "Find the first directory that contains both a \"java\" and \"resources\" directory"
-  (setq path (javares--drop-elements-until-directory path)) ; remove the filename
-  (let ((path (file-truename path)))
-    (while (not (javares--contains-java-and-resources-p path))
-      (setq path (javares--drop-last-element path))
-      (if (equal path "") (error "no resources path ")))
-    path))
-
-(defun javares--java-subtree ()
-  (concat (javares--find-code-basepath (file-truename (buffer-file-name)))
+(defun jproperty-utils--java-subtree ()
+  (concat (jproperty-utils--find-code-basepath (file-truename (buffer-file-name)))
           "/java"))
 
-(defun javares--resource-subtree ()
-  (concat (javares--find-code-basepath (file-truename (buffer-file-name)))
+(defun jproperty-utils--resource-subtree ()
+  (concat (jproperty-utils--find-code-basepath (file-truename (buffer-file-name)))
           "/resources"))
-
-;;;; HIC SUNT LEONES
 
 (defun jproperty-utils-is-file-p (path)
   "Return the path if associated with a readable and writable non link file, nil otherwise"
@@ -61,8 +50,8 @@ Works only on absolute files"
   "Return the resource as a path relative to the current basepath"
   (candidate-path (concat
                    (file-name-as-directory
-                    (javares--resource-subtree))
-                   javares--resource-path)))
+                    (jproperty-utils--resource-subtree))
+                   resource-path)))
 
 (defun jproperty-utils-kill-line ()
   "kill a whole line"
@@ -70,12 +59,18 @@ Works only on absolute files"
     (beginning-of-line)
     (kill-line 1)))
 
+(defun jproperty-utils-delete-file (path)
+  (condition-case nil
+      (not (delete-file path))
+    (error nil)))
+
 (defun jproperty-utils-remove-line-and-file (path)
   "Remove the path and, on success, the line.
 
 Fails with an error if the file cannot be deleted"
-  
-  )
+  (if (jproperty-utils-delete-file path)
+      (jproperty-utils-kill-line)
+    (error (format "Unable to remove file '%s' associated with resource" path))))
 
 (defun jproperty-utils-valid-resource-p ()
   "Returns a cons cell with resource key and value for valid non empty resources, nil otherwise"
